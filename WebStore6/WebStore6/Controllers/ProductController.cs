69dbded5df79ad633815e5c38fed22d689fd6e75
ruslan <ruslans.fjodorovs@gmail.com>
2020-01-27 20:49:45 +0200

    hello world

---
+++ WebStore6/WebStore6/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore6.Models;
+
+namespace WebStore6.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var product = new Product()
+            {
+                Name = "Hello world"
+            };
+            return View(product);
+        }
+    }
+}
